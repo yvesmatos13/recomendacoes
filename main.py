ba6aae18e55587e6a863c5e67b5600c3df8e8f87
Yves Matos <yvesmatos13@gmail.com>
2023-07-31 09:07:26 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+import conexao
+ 
+import json 
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/recomendacoes")
+def getRecomendacoes():
+    recomendacoes = []
+    for recomendacao in conexao.collection.find():
+        recomendacoes.append({"nome":recomendacao['nome'],
+                              "tipo":recomendacao['tipo']})
+    return recomendacoes
